1af48f318658a7785a708692e35601d17c8d2b33
Ayberk Tosun <ayberk.tosun@gmail.com>
2020-04-23 22:54:45 +0200

    Fix syntactic error

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -80,7 +80,7 @@ run_test() {
     elif ! pdftops ${PDFTOPS_ARGS} "${TESTDIR}/main.pdf" "${TESTDIR}/main.ps" ; then
 	echo "${TESTNAME}: pdftops failed, see previous output."
 	return 3
-    elif ! comparepdf --verbose=2 "${TESTDIR}/main.pdf" "${TESTDIR}/main.golden.pdf" then
+    elif ! comparepdf --verbose=2 "${TESTDIR}/main.pdf" "${TESTDIR}/main.golden.pdf" ; then
 	echo "${TESTNAME}: Failed! PDF's don't match."
 	echo "${TESTNAME}: Failed PDF can be found in ${SRCDIR}/main.mismatch.pdf"
 	cp "${TESTDIR}/main.pdf" "${SRCDIR}/main.mismatch.pdf"
